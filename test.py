d940548faafb6d2da7deea5e39a143b20f713d26
Dito Putra <ditoaldi@gmail.com>
2019-10-28 14:06:56 +0700

    image classifier

--- test.py
+++ test.py
@@ -1,14 +1,22 @@
 #image classifier testing
 #dito15
 import pickle
-from skimage.io import imread_collection, imshow
+from skimage.io import imread_collection, imshow, imread
 from skimage.transform import resize
 from skimage.color import rgb2gray
 from skimage import io
-imgtest = imread_collection('images/neg12.gif')
-imgtest = [rgb2gray(x) for x in imgtest]
-imgtest = [resize(x,(77,65),mode='constant', anti_aliasing=False) for x in imgtest]
-imgtest = [x.flatten('C') for x in imgtest]
+imgtest = imread('images/test/test1.gif')
+imgtest = rgb2gray(imgtest)
+imgtest = resize(imgtest,(77,65),mode='constant', anti_aliasing=False)
+imgtest = imgtest.flatten('C')
+imgtest = imgtest.reshape(1,-1)
+print(imgtest)
+
 with open('finalized_model.pkl', 'rb') as file:
     model = pickle.load(file)
+#class prediction
 print('class predicted:', model.predict(imgtest))
+#probability
+prob =  model.predict_proba(imgtest)[:,1]
+prob = float(prob)
+print("{0:.2f}".format(prob))
